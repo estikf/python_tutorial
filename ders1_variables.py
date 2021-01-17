6823d39b7a56fa38fc6efdfceafc3f6f4f129c5e
estikf <estikfurkan@gmail.com>
2021-01-17 13:14:59 +0300

    Ilk Ders

---
+++ ders1_variables.py
@@ -0,0 +1,111 @@
+# Comments
+
+"""
+Çoklu satır yorum.
+
+
+Çoklu satır yorum.
+"""
+    # Tek satır yorum
+
+# Variables
+
+"""
+x = 5                           # integer   
+y = "Furkan&0*_$ajksdkasd"      # string
+z = True                        # Boolean
+v = 5.1                         # Float
+
+print(x)
+print(type(y))
+print(type(z))
+
+my_variable = "String"
+my_number = 5
+print(my_number + 10.3)
+"""
+
+# Global
+
+"""def myFunc():
+    global isim
+    isim = "Alp"
+
+myFunc()
+print(isim)"""
+
+
+# Data Types
+
+"""
+x = "Hello World"	                                    #str
+x = 20                                                  #int	
+x = 20.5	                                            #float
+x = 1j	                                                #complex
+y = [5, "banana", True,5,5,5,5,5,5,5]	                #list
+x = ("apple", "banana", "cherry")	                    #tuple
+x = range(6)                                            #range
+x = {"name" : "John", "age" : 36}	                    #dict
+x = {5, True, "cherry", "apple"}	                    #set
+x = frozenset({"apple", "banana", "cherry"})	        #frozenset	
+x = True	                                            #bool	
+x = b"Hello"	                                        #bytes	
+x = bytearray(5)	                                    #bytearray	
+x = memoryview(bytes(5))	                            #memoryview
+
+my_list = {"name":"Furkan"}
+
+print(my_list["name"])
+"""
+# Random Module
+
+"""
+import random
+print(random.random()*10)
+"""
+
+# Strings
+
+a = """LOREM,LOREM LOREM IPSUM dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididuntut,
+labore et dolore magna aliqua. """
+
+my_list = ["Furkan","Alp","Mehmet","A"]
+
+#print(my_list[0])
+#print(len(my_list))
+#print("Lorem" in a)
+#print("Ahmet" not in a)
+#print(a[-5:-1])
+#print(a.count("LOREM"))
+#print(a.split("i"))
+
+name = "Furkan"
+surname = "Estik"
+
+#print(f"String {name} {surname}")
+
+txt = "We are the so-called from the north."
+#print(txt)
+
+# Booleans
+
+"""print(10>5)
+print(10<5)
+print(10>=5)
+print(10==5)
+print(bool(False))
+
+"""
+
+#Python Assignment Operators
+
+a = 5
+a -= 4
+
+b = [a,"Furkan"]
+
+c = 10
+#print(not(a > b and c < b))
+
