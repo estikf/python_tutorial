17903a6ac1acce0cbc83831232d7f82f14536230
estikf <estikfurkan@gmail.com>
2021-02-14 15:08:15 +0300

    Currency Degisiklik

--- Examples/currency_machine.py
+++ Examples/currency_machine.py
@@ -1,10 +1,10 @@
-SUN_1_SET = False
-SUN_2_SET = True
+SUN_1_SET = True
+SUN_2_SET = False
 SOLAR_OBSERVATION_FEE_MULTIPLIER = 0.05
 COMP202COIN_FLAT_FEE = 10
 COMP202COIN_DOLLAR_EXCHANGE_RATE = 0.05
 DOLLAR_COMP202COIN_EXCHANGE_RATE = 0.01
-COMP202_SUPPLY = "128"
+COMP202_SUPPLY = "64"
 
 def display_welcome_menu():
     
@@ -18,32 +18,32 @@ def display_welcome_menu():
     return display_string
 
 def get_solar_observation_fee(amount_of_comp202coin):
-    if (SUN_1_SET is True) or (SUN_2_SET is True):
+    if (SUN_1_SET is False) or (SUN_2_SET is False):
         return 0
     else:
-        return amount_of_comp202coin * SOLAR_OBSERVATION_FEE_MULTIPLIER
+        return int(amount_of_comp202coin, 16) * SOLAR_OBSERVATION_FEE_MULTIPLIER
     
 def get_flat_fee():
     return COMP202COIN_FLAT_FEE
 
 def convert_COMP202COIN_to_dollars(amount_of_comp202coin):
-    base_10_number = 0
+    base_10_number = int(amount_of_comp202coin, 16)
+
+    """base_10_number = 0
     hex_numbers = "0123456789ABCDEF"
     
     for i,d in enumerate(str(amount_of_comp202coin).upper()):      
         value = hex_numbers.index(d)                            # value (0 - 15)
         power = len(amount_of_comp202coin) - (i+1)              # power 
-        base_10_number += (value * 16 ** power)                 # value * 16 * power
+        base_10_number += (value * 16 ** power)                 # value * 16 * power"""
 
     amount_of_dollar = (                                        # amount of dollar formula
         (base_10_number * COMP202COIN_DOLLAR_EXCHANGE_RATE) 
-        - (get_solar_observation_fee(amount_of_comp202coin) * base_10_number)
-        - COMP202COIN_FLAT_FEE
+        - (get_solar_observation_fee(amount_of_comp202coin))
+        - get_flat_fee()
         )
-    if amount_of_dollar > 0:
-        return amount_of_dollar
-    else:
-        return 0
+
+    return amount_of_dollar
 
 def convert_dollar_to_COMP202COIN(amount_of_dollars):
     actual_money = int(amount_of_dollars * DOLLAR_COMP202COIN_EXCHANGE_RATE)    # dollar multiplied by exchange rate
@@ -51,27 +51,22 @@ def convert_dollar_to_COMP202COIN(amount_of_dollars):
     return result
 
 def get_excess_dollars_after_conversion(amount_of_dollars):
-    
-    coins = convert_dollar_to_COMP202COIN(amount_of_dollars)
-    excess_money = int(str(amount_of_dollars % 100)[-2:])
-    if coins < COMP202_SUPPLY:
-        return excess_money
+
+    excess_money = int(str(amount_of_dollars % 100))
+
+    if excess_money < int(COMP202_SUPPLY, 16):
+        amount_of_coins = convert_dollar_to_COMP202COIN(amount_of_dollars)
+
+        print(f"You have deposited {amount_of_dollars} dollars. You will receive {amount_of_coins} coins ,and {excess_money} dollars will be returned to you.")
     else:
-        return amount_of_dollars
+        print(f"Your transactions cannot be completed due to insufficient funds.")
 
 def operate_machine():
-    while True:
         user_choice = input(display_welcome_menu())
 
         if user_choice == "1":
             amount_of_dollars = int(input("Enter the amount of dollar to convert: "))
-            
-            if amount_of_dollars < int(COMP202_SUPPLY, 16):
-                amount_of_coins = convert_dollar_to_COMP202COIN(amount_of_dollars)
-                excess_money = get_excess_dollars_after_conversion(amount_of_dollars)
-                print(f"You have deposited {amount_of_dollars}. You will receive {amount_of_coins},and {excess_money} dollars will be returned to you.")
-            else:
-                print(f"Your transactions cannot be completed due to insufficient funds.")
+            get_excess_dollars_after_conversion(amount_of_dollars)
 
         elif user_choice == "2":
             amount_of_coins = str(input("Enter the amount of COMP202COIN to convert: "))
@@ -80,7 +75,7 @@ def operate_machine():
 
         elif user_choice == "3":
             print("Program terminated.")
-            break
+
         else:
             print("Please enter a valid choice.")
 
